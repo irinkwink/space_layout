bdb886d751866527862957b79741f8498252c996
irinkwink <irina-ns@yandex.ru>
2024-04-03 20:30:55 +0300

    fix: implement an art button with clip-path

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -15,7 +15,9 @@ export const Button: React.FC<ButtonProps> = ({
       className={styles.button}
       onClick={onClick}
     >
-      {children}
+      <span>
+        {children}
+      </span>
     </button>
   )
 }
